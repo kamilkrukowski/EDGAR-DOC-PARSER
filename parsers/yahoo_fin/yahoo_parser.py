da4d6022ef78714181ef97a110b6377803aa28da
Kamil Krukowski <kamiljkrukowski@gmail.com>
2022-09-27 13:11:36 -0400

    Added apikey parser

--- parsers/yahoo_fin/yahoo_parser.py
+++ parsers/yahoo_fin/yahoo_parser.py
@@ -1,3 +1,13 @@
+from yaml import load, CLoader as Loader
+import os
+
+path = os.path.join(
+        os.path.abspath(os.path.join(__file__, os.pardir)),
+        '../api_keys.yaml')
+
+apikeys = load( open(path, 'rb'), Loader=Loader)
+
+
 import yahoo_fin.stock_info as si
 
 sp = si.tickers_sp500();
