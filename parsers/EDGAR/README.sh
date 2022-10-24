3864380e739ea236e2b9246f0e90d4ce5c02ba3f
kamilkrukowski <Kamiljkrukowski@gmail.com>
2022-10-24 10:43:53 -0400

    Update README.sh

--- parsers/EDGAR/README.sh
+++ parsers/EDGAR/README.sh
@@ -2,3 +2,7 @@
 
 1. Gecko Driver (Firefox)
  ``` wget https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux32.tar.gz ```
+ 
+``` conda create -n edgar scipy numpy selenium ```
+``` pip install sec-edgar ```
+``` pip install beautifulsoup4 ```
