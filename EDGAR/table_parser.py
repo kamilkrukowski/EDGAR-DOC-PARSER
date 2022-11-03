be030206cb439f246176ee1ca05a3f2411b90fb4
yyitingli <66972539+yyitingli@users.noreply.github.com>
2022-11-03 13:09:44 -0400

    Update table_parser.py

--- EDGAR/table_parser.py
+++ EDGAR/table_parser.py
@@ -6,13 +6,159 @@ import pandas as pd
 import numpy as np
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.remote.webelement import WebElement 
+from collections import Counter
+from datetime import datetime
 
 
+def table_parsing_2022(found_table, table_is_numeric, driver):
+    # create a list of numeric table. Each table has its correspond index to the found table.
+    numeric_table = [{"index": index, "element": found_table[index]} for index, val in enumerate(table_is_numeric) if val == 0 ]
+    # dictionary that store the range of y for each page
+    page_location = find_page_location(driver) 
+
+    #find table information
+    for index, val in enumerate(numeric_table) :
+        val["info"] = get_element_info(val["element"])
+        page_num = get_page_number(page_location, val["element"])
+        val["info"]["page_num"] = page_num 
+        val["info"]["location"]["y"] -=  page_location[page_num][0]
+    #find label information
+    for table in numeric_table:
+        table["annotation"] = get_annotated_label_from_table(driver, table, table["info"]["page_num"],page_location[table["info"]["page_num"]][0] )
+        
+    return numeric_table
+
+def table_element_to_df_2022(table_WebElement):
+    table_val, first_label_row = table_to_list_2022(table_WebElement)
+    index_name, column_name_row,value_row = list_of_row_to_df_input_2022(table_val,first_label_row)
+    column_name_row  = pd.MultiIndex.from_tuples(column_name_row)
+    index_name = pd.MultiIndex.from_tuples(index_name)
+    df = pd.DataFrame(value_row , index=index_name, columns=column_name_row)
+    df_dropna = df.dropna(axis='columns', how ="all")
+    
+    return df_dropna   
+
+#@func_running_time
+def list_of_table_element_to_df_2022(numeric_table:list()):
+    df_numeric_table = []
+    for i in range(len(numeric_table)):
+        df_numeric_table.append(table_element_to_df(numeric_table[i]["element"]))
+    return df_numeric_table
+
+#####################################################################################################################################################
+#
+#
+#
+#                                              Helper Function 
+#
+#
+#
+#
+#####################################################################################################################################################
+# a decorator to measure the time of a function
+def func_running_time(func):
+    def inner(*args, **kwargs):
+        print(f'INFO Begin to run function: {func.__name__} …')
+        time_start = datetime.now()
+        res = func(*args, **kwargs)
+        time_diff = datetime.now() - time_start
+        print(f'INFO Finished running function: {func.__name__}, total: {time_diff.seconds}s')
+        print()
+        return res
+    return inner
+
+#@func_running_time
+def table_to_list_2022(table):
+    tr = table.find_elements(By.TAG_NAME, 'tr')
+    colspan= tr[0].find_elements(By.TAG_NAME, 'td')[0].get_attribute("colspan")
+    
+    table_val = []
+    label_index =  0
+    found_label= False
+    
+    for index  in range(1,len(tr)):
+        col_index = 0
+        val = [None] * int(colspan)
+        row = tr[index]
+        
+        if row.text in {""," "}:
+            continue
+        
+        if (found_label==  False ):
+            fraction = len(row.find_elements(By.TAG_NAME, 'ix:nonfraction'))
+            if(fraction>0):
+                found_label = index
+            
+        row = row.find_elements(By.TAG_NAME, 'td')
+        
+        for i, td in enumerate(row):
+            if td == None:
+                continue
+            temp_colspan = td.get_attribute("colspan")
+            
+            if(td.text in {")","",'',"%","(","$"}): 
+                val[col_index] = None
+            else:
+                val[col_index] = td.text.replace("\n", "").replace("( ", "")
+            
+            col_index = ( col_index + 1 ) if temp_colspan == None else ( col_index + int(temp_colspan) )
+            
+        label_index+= 1
+        table_val.append(val)       
+        
+    return table_val, found_label
+
+
+#@func_running_time
+def list_of_row_to_df_input_2022(table_val, first_label_row):
+    num_row = len(table_val)
+    #get the index name
+    index_name = []
+    column_name_row = []
+    value_row =[]
+    #get the column name
+    row_index_start = 0
+    
+    for index_col_name_row in range(num_row):
+        temp_val = None
+        row = table_val[index_col_name_row][1:]
+        if(table_val[index_col_name_row][0] != None or first_label_row == index_col_name_row ):
+            row_index_start = index_col_name_row
+            break
+        else:
+            for i in range(len(row)):
+
+                if row[i] != None:
+                    row[i-1] = row[i] if i != 0 else row[i-1]
+                    temp_val = row[i]
+
+                else:
+                    row[i] = temp_val
+        column_name_row.append(row)
+        
+    #get the value
+    column_name_row = list(zip(*column_name_row))
+    max_type_column = len(Counter(column_name_row))
+    for index_val_row in range(row_index_start,num_row):
+        index_name.append((index_val_row-row_index_start,table_val[index_val_row][0]))
+        value = table_val[index_val_row][1:]
+        length_value = len(value)
+       
+        for i in range(0,length_value,int(np.ceil(length_value/max_type_column))):
+            if(value[i] == None):
+                for x in range(i+1,i + int(np.ceil(length_value/max_type_column)),1):
+                    if(x >= len(row)):
+                        continue
+                    if (value[x] != None):
+                        value[i],value[x] = value[x], None
+                        continue
+        value_row.append(value)
+            
+        
+    return index_name, column_name_row,value_row
+        
 def get_element_info(element: WebElement)-> list():
     return {"value": element.text,"location": element.location, "size": element.size}
-
-def pause() -> None:
-    programPause = input("Press the <ENTER> key to close the selenium webdriver...")
     
 
 def border(elem, driver: webdriver, color:str = "red") -> None:
@@ -22,7 +168,6 @@ def border(elem, driver: webdriver, color:str = "red") -> None:
     
 def find_page_location(driver: webdriver) -> dict:
     page_breaks = driver.find_elements(By.TAG_NAME, 'hr')
-    #print("length of page breaks",len(page_breaks))
     page_number = 1
     # get the range of y for page 1
     page_location = {page_number: [0,page_breaks[0].location["y"]]}
@@ -62,12 +207,8 @@ def get_annotated_label_from_table(driver: webdriver, table: dict, page_num = fl
         annotations[-1]["info"]["location"]["y"] -= page_num_y
     return annotations
 
-def table_parsing_2022(found_table, table_is_numeric, driver):
-    # create a list of numeric table. Each table has its correspond index to the found table.
-    numeric_table = [{"index": index, "element": found_table[index]} for index, val in enumerate(table_is_numeric) if val == 0 ]
-    # dictionary that store the range of y for each page
-    page_location = find_page_location(driver) 
 
+<<<<<<< Updated upstream
     #find table information
     for index, val in enumerate(numeric_table) :
         val["info"] = get_element_info(val["element"])
@@ -79,3 +220,5 @@ def table_parsing_2022(found_table, table_is_numeric, driver):
         table["annotation"] = get_annotated_label_from_table(driver, table, table["info"]["page_num"],page_location[table["info"]["page_num"]][0] )
         
     return numeric_table
+=======
+>>>>>>> Stashed changes
