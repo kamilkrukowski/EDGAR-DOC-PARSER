aef9d6c79cb8c48662322bbabc75ccf6e15ac0b7
yyitingli <66972539+yyitingli@users.noreply.github.com>
2022-11-15 22:00:12 -0500

    Update generate_dataset_nflx.py

--- setup/generate_dataset_nflx.py
+++ setup/generate_dataset_nflx.py
@@ -1,9 +1,9 @@
 """
-    This file will featurize the 10-ticker companies and saved the cached feature pandas dataframe
+    This file will featurize the nflx and saved the cached feature pandas dataframe
     That dataframe can be loaded for downstream transformations, such as selection of subfields which is done below and saved in a sample_data.csv
 """
 import os
-import sys ; sys.path.append('../')
+import sys ; sys.path.append('..')
 
 
 from tqdm.auto import tqdm
@@ -14,49 +14,67 @@ import EDGAR
 
 
 data_dir = os.path.join('..', 'data')
-api_keys_path = os.path.join('..','api_keys.yaml')
 
-metadata = EDGAR.Metadata(data_dir=data_dir)
-loader = EDGAR.dataloader(data_dir=data_dir, api_keys_path=api_keys_path,metadata=metadata);
-parser = EDGAR.parser(data_dir=data_dir, metadata=metadata)
+metadata = EDGAR.metadata(data_dir=data_dir)
+loader = EDGAR.downloader(data_dir=data_dir);
+parser = EDGAR.parser(data_dir=data_dir)
 
-tikr = "nflx"
 
-metadata.load_tikr_metadata(tikr)
-annotated_docs = parser.get_annotated_submissions(tikr)
+# List of companies to process
+#tikrs = open(os.path.join(loader.path, '..', 'tickers.txt')).read().strip()
+#tikrs = [i.split(',')[0].lower() for i in tikrs.split('\n')]
+
+tikrs = ["nflx"]
 
 trainset = []
 
-for doc in tqdm(annotated_docs, desc=f"Processing {tikr}", leave=False):
-    fname = metadata.get_10q_name(tikr, doc)
-    features = parser.process_file(tikr, doc, fname) 
 
-    found_indices = np.unique([int(i) for i in features['found_index']])
-    data = {i:{'text':None, 'labels':dict(), 'labelled':False } for i in found_indices}
-    
-    for i in range(len(features)):
-        i = features.iloc[i, :]
-        if not i['is_annotation']:
-            continue;
-        d = data[i['found_index']]
-        d['labelled'] = True
-        if d['text'] is None:
-            d['text'] = [i['value'], i["full_text"]]
+for tikr in tikrs:
+    metadata.load_tikr_metadata(tikr)
+    annotated_docs = parser.get_annotated_submissions(tikr)
 
-        d['labels'][i['annotation_index']] = []
-        for _attr in ['name', 'id', 'contextref', 'decimals', 'format', 'unitref']:
-            d['labels'][i['annotation_index']].append(i[_attr])
-    for i in data:
-        d = data[i]
-        if not d['labelled']:
-            continue; 
-        trainset.append((d['text'], list(d['labels'].values())))
+    for doc in tqdm(annotated_docs, desc=f"Processing {tikr}", leave=False):
+        trainset_page_tikr = []
+        fname = metadata.get_10q_name(tikr, doc)
+        # Try load cached, otherwise regenerate new file
+        features = parser.process_file(tikr, doc, fname) 
+        print(features[-1]["page_number"])
+
+        found_indices = np.unique([int(i) for i in features['found_index']])
+        # Structure: Text str, Labels dict, labelled bool
+        data = {i:{'text':None, 'labels':dict(), 'labelled':False } for i in found_indices}
+        trainset_doc = []
+        for i in range(len(features)):
+            i = features.iloc[i, :]
+
+            # Skip documents which are NOT annotated
+            if not i['is_annotation']:
+                continue;
+            d = data[i['found_index']]
+            d['labelled'] = True
+            if d['text'] is None:
+                d['text'] = [i['value'], i["full_text"]]
+
+            d['labels'][i['annotation_index']] = []
+            for _attr in ['name', 'id', 'contextref', 'decimals', 'format', 'unitref']:
+                d['labels'][i['annotation_index']].append(i[_attr])
+
+        # Add all labelled documents to trainset
+        for i in data:
+            d = data[i]
+            if not d['labelled']:
+                continue; 
+            # Data format: (x,y) where x refers to list of training features (present for unnannotated docs), and y refers to list of labels to predict
+            trainset_page_tikr.append((d['text'], list(d['labels'].values())))
+
+
+
+            
+
+        with open(os.path.join('..','outputs','sample_data_nflx.csv'), 'w') as f:
+            text, labels = trainset[0]
+            f.write(f"{text},{':'.join([str(i) for i in labels])}")
+            for text, labels in trainset[1:]:
+                f.write(f"\n{text},{';'.join([str(i) for i in labels])}")
+
         
-
-    with open(os.path.join('..','outputs','sample_data_nflx.csv'), 'w') as f:
-        text, labels = trainset[0]
-        f.write(f"{text},{':'.join([str(i) for i in labels])}")
-        for text, labels in trainset[1:]:
-            f.write(f"\n{text},{';'.join([str(i) for i in labels])}")
-
-    
