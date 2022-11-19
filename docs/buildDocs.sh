820d2d3bf24b2aace662be4b49284ba52bf81202
Kamil Krukowski <kamiljkrukowski@gmail.com>
2022-11-18 20:14:13 -0500

    == pip version

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -18,7 +18,7 @@ set -x
  
 apt-get update
 apt-get -y install git rsync python3-sphinx-rtd-theme python3-stemmer python3-git python3-pip python3-virtualenv python3-setuptools
-python3 -m pip install sphinx=5.3.0
+python3 -m pip install sphinx==5.3.0
 python3 -m pip install pyyaml
 
  
