7d496281163d42af43119744930b0694c24a7c19
yyitingli <66972539+yyitingli@users.noreply.github.com>
2022-11-20 09:55:25 -0500

    #53

--- examples/test_text_by_page.py
+++ examples/test_text_by_page.py
@@ -17,22 +17,24 @@ import EDGAR
 
 
 # Hyperparameters
-tikr = 'nflx'
-submission_date = '20210101'
-headless = True
 data_dir = os.path.join('..','data')
+submission_date = "20210630"
+tikr = 'nflx'
+#EDGAR.set_data_dir(os.path.join('..','data'))
+
+pipeline = EDGAR.preprocesser(data_dir=data_dir)
 
 # Set up
 loader = EDGAR.downloader(data_dir=data_dir)
 loader.metadata.load_tikr_metadata(tikr)
 
+parser = EDGAR.parser(data_dir=data_dir)
+
 # Get nearest 10Q form path to above date
-dname = loader.get_nearest_date_filename(submission_date, tikr)
-fname = loader.metadata.get_10q_name(tikr, dname)
+submission = loader.get_nearest_date_filename(tikr, submission_date)
+filename = loader.metadata.get_10q_name(tikr, submission)
+driver_path = parser.get_driver_path(tikr, submission, filename)
 
-parser = EDGAR.parser(data_dir=data_dir, headless=headless)
-
-driver_path =  parser.get_driver_path(tikr, dname, fname)
 
 ########### testing for parse_text_by_page ##################
 
