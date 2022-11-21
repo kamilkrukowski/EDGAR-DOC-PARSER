58f798b230309b3a10017e475c72834072a6e361
yyitingli <66972539+yyitingli@users.noreply.github.com>
2022-11-21 14:07:54 -0500

    Update generate_dataset.py

--- setup/generate_dataset.py
+++ setup/generate_dataset.py
@@ -38,46 +38,68 @@ for tikr in tikrs:
  
     for doc in tqdm(annotated_docs, desc=f"Processing {tikr}", leave=False):
         fname = metadata.get_10q_name(tikr, doc)
-
         # Try load cached, otherwise regenerate new file
-        features = parser.featurize_file(tikr, doc, fname, force=args.force) 
+        features = parser.featurize_file(tikr, doc, fname,force = True) 
+        features.sort_values(by=['page_number'])
+
+        num_page = features.iloc[0]["page_number"]
     
+
         found_indices = np.unique([int(i) for i in features['found_index']])
         # Structure: Text str, Labels dict, labelled bool
-        data = {i:{'text':None, 'labels':dict(), 'is_annotated':False, 'in_table':False } for i in found_indices}
+        data = {i:{'text':None, 'labels':dict(), 'is_annotated':False, 'in_table':False, "page_number": 0 } for i in found_indices}
         
         for i in range(len(features)):
             i = features.iloc[i, :]
             d = data[i['found_index']]
-            
+            # Skip documents which are NOT annotated
             if i['in_table']:
                 d['in_table'] = True
             if i['is_annotated']:
                 d['is_annotated'] = True
+
             
+         
+            d['page_number'] = i["page_number"]
             if d['text'] is None:
-                d['text'] = i['full_text']
-            d['labels'][i['anno_index']] = i['anno_name']
-        
+                """
+                x is a list with length of 2. Items in the list are:
+                    1. value: the text value of the annotated label (e.g. 10-Q)
+                    2. neighboring text: the text on the given page.
+                y is a list with ['name', 'id', 'contextref', 'decimals', 'format', 'unitref'] tags
+                """
+                d['text'] = i['anno_text']
+
+            d['labels'][i['anno_index']] = []
+            for _attr in ['name', 'id', 'contextref', 'decimals', 'format', 'unitref']:
+                d['labels'][i['anno_index']].append(i["anno_" + _attr])
+
+
+        # Add all labelled documents to trainset
+        data_per_page = [ [] for i in range(num_page)]
         # Add all labelled documents to trainset
         for i in data:
             #Only add labelled documents to trainset
             if not data[i]['is_annotated'] or data[i]['in_table']:
                 continue; 
             d = data[i]
-            # Data format: (x,y) where x refers to training features (present for unnannotated docs), and y refers to labels to predict
-            # TODO: Convert to list of [lists of tuples, page number, parent_company_tikr] scheme where each list of tuples consists of all annotated webelements on that page
-            trainset.append((d['text'], list(d['labels'].values())))
             
-    
-        # Sample CSV with text, tag information
-        with open(os.path.join('..','outputs','sample_data.csv'), 'w') as f:
-            if len(trainset) > 0:
-                text, labels = trainset[0]
-                f.write(f"{text},{':'.join([str(i) for i in labels])}")
-                for text, labels in trainset[1:]:
-                    f.write(f"\n{text},{';'.join([str(i) for i in labels])}")
-            else:
-                warnings.warn('No Trainset Samples', RuntimeWarning)
+            # Data format: (x,y) where x refers to training features (present for unnannotated docs), and y refers to labels to predict
+            data_per_page[d['page_number']-1].append((d['text'], d['labels'].values()))
 
-    
+        # TODO: Convert to list of [lists of tuples, page number, document,parent_company_tikr] scheme where each list of tuples consists of all annotated webelements on that page
+        for i , d in enumerate(data_per_page ):
+            # Only add list if the list is not empty
+            if len(d) == 0:
+                continue
+            trainset.append([ d, i+1,doc, tikr  ])
+
+with open(os.path.join('..','outputs','sample_data.csv'), 'w') as f:
+    for page in trainset: 
+        f.write(f"Page_number: {page[1]},document:{page[2]},Tikr:{page[3]}\n")
+        data = page[0]
+        text, labels = data[0]
+        f.write(f"{text},{':'.join([str(i) for i in labels])}")
+        for text, labels in data[1:]:
+            f.write(f"\n{text},{';'.join([str(i) for i in labels])}")
+        f.write(f"\n\n")
