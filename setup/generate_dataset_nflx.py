ea2e29134c46b62769e49a1aea36e386374e58e7
yyitingli <66972539+yyitingli@users.noreply.github.com>
2022-11-22 16:01:50 -0500

    Update generate_dataset_nflx.py

--- setup/generate_dataset_nflx.py
+++ setup/generate_dataset_nflx.py
@@ -10,71 +10,50 @@ from tqdm.auto import tqdm
 import numpy as np
 import argparse
 import warnings
-
+import pandas as pd
 
 import EDGAR
 
 
 # Command line magic for common use case to regenerate dataset
 #   --force to overwrite outdated local files
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--force', action='store_true')
-args = parser.parse_args()
 
-data_dir = os.path.join('..', 'data')
+def _generate_dataset(features: pd.DataFrame, doc, tikr ):
+    trainset =[]
+    features.sort_values(by=['page_number'])
 
-metadata = EDGAR.metadata(data_dir=data_dir)
-loader = EDGAR.downloader(data_dir=data_dir);
-parser = EDGAR.parser(data_dir=data_dir)
-
-tikrs = ["nflx"]
-
-trainset = []
+    num_page = max(features.iloc[:]["page_number"], default=0)
 
 
-for tikr in tikrs:
-    metadata.load_tikr_metadata(tikr)
-    annotated_docs = parser.get_annotated_submissions(tikr)
-    annotated_docs = [annotated_docs[0]]
-    for doc in tqdm(annotated_docs, desc=f"Processing {tikr}", leave=False):
-        fname = metadata.get_10q_name(tikr, doc)
-        # Try load cached, otherwise regenerate new file
-        features = parser.featurize_file(tikr, doc, fname,force = args.force) 
-        features.sort_values(by=['page_number'])
+    found_indices = np.unique([int(i) for i in features['found_index']])
+    # Structure: Text str, Labels dict, labelled bool
+    data = {i:{'text':None, 'labels':dict(), 'is_annotated':False, 'in_table':False, "page_number": 0 } for i in found_indices}
 
-        num_page = max(features.iloc[:]["page_number"], default=0)
-    
 
-        found_indices = np.unique([int(i) for i in features['found_index']])
-        # Structure: Text str, Labels dict, labelled bool
-        data = {i:{'text':None, 'labels':dict(), 'is_annotated':False, 'in_table':False, "page_number": 0 } for i in found_indices}
-        page_texts = features[features["is_annotated"]== 1][["page_number", "page_text"]]
-        page_texts = page_texts.drop_duplicates(subset = "page_number", keep = "last").set_index("page_number")
-  
-        for i in range(len(features)):
-            i = features.iloc[i, :]
-            d = data[i['found_index']]
-            # Skip documents which are NOT annotated
-            if i['in_table']:
-                d['in_table'] = True
-            if i['is_annotated']:
-                d['is_annotated'] = True
+    for i in range(len(features)):
+        i = features.iloc[i, :]
+        d = data[i['found_index']]
+        # Skip documents which are NOT annotated
+        if i['in_table']:
+            d['in_table'] = True
+        if i['is_annotated']:
+            d['is_annotated'] = True
 
-            
         
-            d['page_number'] = i["page_number"]
-            if d['text'] is None:
-                """
-                x is a list with length of 2. Items in the list are:
-                    1. value: the text value of the annotated label (e.g. 10-Q)
-                    2. neighboring text: the text on the given page.
-                y is a list with ['name', 'id', 'contextref', 'decimals', 'format', 'unitref'] tags
-                """
-                d['text'] = i['anno_text']
+    
+        d['page_number'] = i["page_number"]
+        if d['text'] is None:
+            """
+            x is a list with length of 2. Items in the list are:
+                1. value: the text value of the annotated label (e.g. 10-Q)
+                2. neighboring text: the text on the given page.
+            y is a list with ['name', 'id', 'contextref', 'decimals', 'format', 'unitref'] tags
+            """
+            d['text'] = i['span_text']
 
-            d['labels'][i['anno_index']] = []
-            for _attr in ['name', 'id', 'contextref', 'decimals', 'format', 'unitref']:
-                d['labels'][i['anno_index']].append(i["anno_" + _attr])
+        d['labels'][i['anno_index']] = []
+        for _attr in ['name', 'id', 'contextref', 'decimals', 'format', 'unitref']:
+            d['labels'][i['anno_index']].append(i["anno_" + _attr])
 
 
         data_per_page = [ [] for i in range(num_page)]
@@ -93,7 +72,31 @@ for tikr in tikrs:
 
             if len(d) == 0:
                 continue
-            trainset.append([ d, i+1,doc, tikr,page_texts.loc[i+1]["page_text"]  ])
+            trainset.append([ d, i+1,doc, tikr ])
+
+    return trainset
+
+def generate_dataset_nflx():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--force', action='store_true')
+    args = parser.parse_args()
+
+    data_dir = os.path.join('..', 'data')
+
+    metadata = EDGAR.metadata(data_dir=data_dir)
+    loader = EDGAR.downloader(data_dir=data_dir);
+    parser = EDGAR.parser(data_dir=data_dir)
+    trainset = []
+    for tikr in ["nflx"]:
+        metadata.load_tikr_metadata(tikr)
+        annotated_docs = parser.get_annotated_submissions(tikr)
+        annotated_docs = [annotated_docs[0]]
+        for doc in tqdm(annotated_docs, desc=f"Processing {tikr}", leave=False):
+            fname = metadata.get_10q_name(tikr, doc)
+            # Try load cached, otherwise regenerate new file
+            features = parser.featurize_file(tikr, doc, fname,force = args.force) 
+            trainset +=_generate_dataset(features, doc, tikr )
+    return trainset    
 
 
 """
@@ -132,10 +135,12 @@ Training set
                 warnings.warn('No Trainset Samples', RuntimeWarning)
 '''
 
+trainset = generate_dataset_nflx()
 with open(os.path.join('..','outputs','sample_data_nflx.csv'), 'w') as f:
     for page in trainset: 
-        f.write(f"Page_number: {page[1]},document:{page[2]},Tikr:{page[3]},Text:{page[4]}\n")
+        f.write(f"Page_number: {page[1]},document:{page[2]},Tikr:{page[3]}\n")
         data = page[0]
+        #print(data)
         text, labels = data[0]
         f.write(f"{text},{':'.join([str(i[0]) for i in labels])}")
         for text, labels in data[1:]:
