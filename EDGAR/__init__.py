d33f9e8eb30204561e0b84950778c4f831f15365
yyitingli <66972539+yyitingli@users.noreply.github.com>
2023-02-09 10:37:56 -0800

    Update __init__.py

--- EDGAR/__init__.py
+++ EDGAR/__init__.py
@@ -5,7 +5,6 @@ from . import metadata_manager as edgar_metadata
 from . import downloader as edgar_downloader
 from . import preprocesser as edgar_preprocesser
 from . import parser as edgar_parser
-from . import data_subset as edgar_subset
 
 
 class EDGAR_singleton:
@@ -45,14 +44,6 @@ class EDGAR_singleton:
             self.preprocesser = edgar_preprocesser.edgar_preprocesser(*args, **kwargs)
         return self.preprocesser
 
-    def _get_subset(self,reuse: bool = True, *args, **kwargs):
-        if 'metadata' not in kwargs:
-            kwargs['metadata'] = self._get_metadata(data_dir=kwargs.get('data_dir', 'data'), reuse=reuse)
-        if 'parser' not in kwargs:
-            kwargs['parser'] = self._get_parser(data_dir=kwargs.get('data_dir', 'data'), reuse=reuse)
-        if self.edgar_subset is None or not reuse:
-            self.edgar_subset = edgar_subset.DataSubset(*args, **kwargs)
-        return self.edgar_subset
     
 
 edgar_global = EDGAR_singleton()
@@ -61,4 +52,3 @@ metadata = edgar_global._get_metadata
 downloader = edgar_global._get_downloader
 parser = edgar_global._get_parser
 preprocesser = edgar_global._get_preprocesser
-subset = edgar_global._get_subset
